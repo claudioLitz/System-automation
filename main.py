8fb135d92a245296c213936d697425322a62a208
ClaudioLitz <claudio_litz@estudante.sesisenai.org.br>
2026-03-24 10:22:43 -0300

    Update main.py

--- main.py
+++ main.py
@@ -11,45 +11,96 @@ import pyperclip
 import time
 import pandas as pd
 
-# Step 0 - Program start alert
-py.alert("Espere o programa terminar para voltar a mexer")
+def OpenChrome():
+    # ---------------------------------------- Step 1 - Acess interprise system ----------------------------
+    # Initialize Chrome browser
+    Browser = webdriver.Chrome()
+
+    # Open a site using URL
+    Browser.get("https://pages.hashtagtreinamentos.com/aula1-intensivao-sistema")
+
+    # Maxize window
+    py.hotkey('win', 'up')
+
+def Login():
+    # ---------------------------------------- Step 2 - Login on system --------------------------------------
+    # Login (User name)
+    py.press('tab')
+    py.write('Claudio_litz')
+    time.sleep(1)
+
+    # Login (Password)
+    py.press('tab')
+    py.write('senhaforte')
+    time.sleep(1)
+
+    # Enter to acess
+    py.press(['tab','enter'])
+    time.sleep(3)
+
+def download():
+    # ---------------------------------------- Step 3 - Download DB ----------------------------------------------
+    py.click(1181,339)
+    time.sleep(3)
+    py.alert("Downloaded file? ")
 
 
-# ---------------------------------------- Step 1 - Acess interprise system ----------------------------
-# Initialize Chrome browser
-navegador = webdriver.Chrome()
+def CalculateIndicators():
+    # ---------------------------------------- Step 4 - Calculate indicators --------------------------------------
+    # DB import
+    table = pd.read_csv(r"C:\Users\claudio_litz\Downloads\Compras.csv", sep=';')
+    print(table)
 
-# Open a site using URL
-navegador.get("https://pages.hashtagtreinamentos.com/aula1-intensivao-sistema")
 
-# Maxize window
-py.hotkey('win', 'up')
+    total_spent = table["ValorFinal"].sum()
+    quantity = table["Quantidade"].sum()
+    avarage_price = table["ValorUnitário"].mean()
 
-# ---------------------------------------- Passo 2 - Fazer login nos sistemas --------------------------------------
-# Login (User name)
-py.press('tab')
-py.write('Claudio_litz')
-time.sleep(1)
-
-# Login (Password)
-py.press('tab')
-py.write('senhaforte')
-time.sleep(1)
-
-# Pressionar Enter
-py.press(['tab','enter'])
-time.sleep(3)
-
-# ---------------------------------------- Passo 3 - Baixar a base de dados ----------------------------------------------
-py.click(1181,339)
-time.sleep(1)
-py.alert("Arquivo baixado? ")
-
-# ---------------------------------------- Passo 4 - Calcular os indicadores
-# Importe da base de dados
-tabela = pd.read_csv(r"C:\Users\claudio_litz\Downloads\Compras.csv", sep=';')
-print(tabela)
+    print(total_spent)
+    print(quantity)
+    print(avarage_price)
 
 
 
-input("Pressione Enter para fechar o navegador...")
+def __init__():
+    # Step 0 - Program start alert
+    py.alert("Wait program finish to back")
+
+    OpenChrome()            # Fuction that will open Chrome browser
+    Login()                 # Fuction to login in enterprise system
+    download()              # Function to download DB
+    CalculateIndicators()   # Function to calculate an indicators
+
+    input("Press enter to finish program...")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+__init__()
